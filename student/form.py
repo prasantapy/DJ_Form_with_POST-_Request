68869f90cf91b75c367834e8ad9636c73605fcd7
prasantapy <prasanta.sethi.py@gmail.com>
2025-06-19 18:53:12 +0530

    Update form.py

--- student/form.py
+++ student/form.py
@@ -2,7 +2,7 @@ from django import forms
 
 class Show_detelis(forms.Form):
     NAME=forms.CharField(
-        label='Enter name ',
+        label='Enter name ',  
     )
     ROLL_number=forms.IntegerField()
     city = forms.CharField(initial='Bhubaneswar')
